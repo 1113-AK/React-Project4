8d4ffa1f66e8d71c17300d2d52c067cc7a16431b
1113-AK <140528276+1113-AK@users.noreply.github.com>
2023-07-31 15:34:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<Home/>,document.getElementById('root'))
+
+
